42072f7f102896014b803bb326f611a8bce9331d
zhukaijun0629 <kaijun.zhu0629@gmail.com>
2021-01-05 14:07:22 -0500

    Upload Materials

---
+++ Course1_Week4/image-transform-given-files_20190225/uiuc/HSLAPixel.h
@@ -0,0 +1,25 @@
+/**
+ * @file HSLAPixel.h
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+namespace uiuc {
+
+  // Put your HSLAPixel class definition here.
+  // (Remember to end it with a semicolon!)
+  class HSLAPixel {
+    public:
+    double h; /**< Hue of the pixel, in degrees [0, 360). */
+    double s; /**< Saturation of the pixel, [0, 1]. */
+    double l; /**< Luminance of the pixel, [0, 1]. */
+    double a; /**< Alpha of the pixel, [0, 1]. */
+  };
+
+}
